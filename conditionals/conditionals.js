7f6c9df8dc0cb218930fda8eeeb483279d40b615
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-06 17:11:33 +0700

    Add chapter conditional

---
+++ conditionals/conditionals.js
@@ -0,0 +1,21 @@
+const age = 15;
+if (age >=18) {
+    console.log('Sarah can start driving');
+} else {
+    const yearLest = 18 -age;
+    console.log(`Sarah is too young. Wait another$ ${yearLest} year`);
+}
+
+const birthday = 2012;
+let century;
+if (birthday <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+const agge = 26;
+const beverage = age >= 21 ? 'Beer' : 'Juice';
+console.log(beverage);
+
