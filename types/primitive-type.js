9692c723d97411ceab2124c406aab109fa358f84
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-06 17:09:55 +0700

    Add primitive type

---
+++ types/primitive-type.js
@@ -0,0 +1,27 @@
+// numbers
+let a = 100;
+console.log(a);
+console.log(typeof a);
+
+// string
+let firstName = "Jonas";
+console.log(firstName);
+console.log(typeof firstName);
+
+// boolean
+let fullAge = true;
+console.log(fullAge);
+console.log(typeof "fullAge");
+fullAge = false;
+console.log(fullAge);
+console.log(typeof "fullAge");
+
+// undefind
+let name;
+console.log(name);
+console.log(typeof name);
+
+// Object type
+const person = { firstName: "Jonas", lastName: "John", age: "35" };
+console.log(person);
+console.log(typeof person);
