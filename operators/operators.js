941d3a86dc3507ab1ce1499ba37f248b24a12375
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-06 17:10:32 +0700

    Add chapter operators

---
+++ operators/operators.js
@@ -0,0 +1,19 @@
+// operators
+const a = 348;
+const b = 14;
+console.log(a - b);
+console.log(a * 2);
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + "" + lastName);
+
+let x = 10 + 5;
+x += 10; // x =  x + 10 = 25
+x *= 4; // x = x * 4 = 25 * 4 = 100
+x++; // x = x + 1;
+x--;
+console.log(x);
+
+// comperision operators
+console.log(x > 18);
