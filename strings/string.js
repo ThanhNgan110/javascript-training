9b980ad58e0d7aba111e5c22d9adf6b4cc7f7c26
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-08 17:29:43 +0700

    Update chapter string

---
+++ strings/string.js
@@ -0,0 +1,13 @@
+const string1 =  "A string primitive";
+
+// length()
+string1.length();
+console.log(string1.length());
+ // character access
+ string1.charAt(s);
+
+ // template literal
+
+const a = 5;
+const b = 10;
+console.log(`Fifteen is ${a + b}`)
