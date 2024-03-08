1d08710a52b8c0917b4ee193f6e986afccea3b2f
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-08 17:29:27 +0700

    Update chapter Object

---
+++ Objects/object.js
@@ -0,0 +1,14 @@
+const jonas = {
+	firstName: "Jonas",
+	lastName: "Schmedtmann",
+	age: 1991,
+	job: "teacher",
+	friends: ["Michael", "Peter"],
+};
+
+console.log(jonas);
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+
+
