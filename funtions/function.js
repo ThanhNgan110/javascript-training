be47a7af25247d6c8574be65f498f76f4212587a
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-11 11:10:02 +0700

    Update chapter function

--- funtions/function.js
+++ funtions/function.js
@@ -29,3 +29,16 @@ const Course =  function (name, price) {
 
 const jsCourse = new Course ('Javascript', 1000);
 console.log(jsCourse);
+
+let user = { 
+    name: "GFG", 
+    gfg1:() => { 
+        console.log(this.name)
+        console.log("hello " + this.name); // no 'this' binding here 
+    }, 
+    gfg2(){        
+        console.log("Welcome to " + this.name); // 'this' binding works here 
+    }   
+ }; 
+user.gfg1(); 
+user.gfg2(); 
