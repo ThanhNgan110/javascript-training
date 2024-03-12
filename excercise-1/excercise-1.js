e993441f37e5414592276cd1dcb6b51d999d3c55
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-12 16:25:57 +0700

    Add excercise one

---
+++ excercise-1/excercise-1.js
@@ -0,0 +1,7 @@
+// The Logo Hijack
+const image = document.getElementById("hplogo");
+image.src = "http://www.logostage.com/logos/yahoo.GIF";
+const button = document.getElementById("gbqfba");
+button.innerHTML = "Yahoooo!";
+
+// About Me
