20bcf920de68ad4847170a137623a986f19f28c4
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-19 15:24:19 +0700

    Add selector for helper

---
+++ src/js/helper/selector.js
@@ -0,0 +1,9 @@
+// Retrieve an element from the DOM
+export const getElement = (selctor) => {
+  const element = document.querySelector(selctor);
+  console.log(element);
+  return element;
+}
+
+// export const createElement ()
+
