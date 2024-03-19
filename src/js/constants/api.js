1ea646ce5beb66bd94539b6264573c3d4e2e056c
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-19 22:03:08 +0700

    Update format code

--- src/js/constants/api.js
+++ src/js/constants/api.js
@@ -1,7 +1,6 @@
 const api = {
   URL_API: "https://puyugolpluagelcytrfx.supabase.co/rest/v1",
-  API_KEY:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eXVnb2xwbHVhZ2VsY3l0cmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MTE5NzMsImV4cCI6MjAyNjM4Nzk3M30.3sQ8ZiP3mChvptClo55CDjXosoJa52QB-McugWuoHQo",
+  API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eXVnb2xwbHVhZ2VsY3l0cmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MTE5NzMsImV4cCI6MjAyNjM4Nzk3M30.3sQ8ZiP3mChvptClo55CDjXosoJa52QB-McugWuoHQo",
 };
 
 export { api };
