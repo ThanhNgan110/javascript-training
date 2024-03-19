5c6912c037cc633b39c104d60b7af4eeab80b9db
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-20 00:52:44 +0700

    Update funtion get element selector

--- src/js/helper/selector.js
+++
@@ -1,8 +0,0 @@
-// Retrieve an element from the DOM
-export const getElement = (selector) => {
-  const element = document.querySelector(`.${selector}`);
-  console.log(element, selector);
-  return element;
-};
-
-// export const createElement ()
