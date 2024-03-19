c79d8412008c08944755be3b9977d1f2c685b500
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-19 17:31:56 +0700

    Update  productview

--- src/js/views/ProductView.js
+++ src/js/views/ProductView.js
@@ -6,22 +6,25 @@ export class ProductView {
   }
 
   renderProduct(products) {
-    let contentProduct = '';
-   contentProduct = products.forEach((item) => {
-      `
-      <article class="card-product">
-      <img class="card-img" src="${products.product_img}">
-      <div class="card-body">
-        <div class="card-content">
-          <h3 class="product-name">${products.product_name}</h3>
-          <p class="product-price">${products.product_price}</p>
-        </div>
-        <span class="border-circle">
-          <span class="icon icon-bag"></span>
-        </span>
-      </div>
-    </article>`;
+    // console.log(products);
+    // console.log(this.cardBlock);
+    let contentProduct = "";
+    products.forEach((item) => {
+      // console.log("abc", item.product_name);
+      contentProduct += `
+            <article class="card-product">
+            <img class="card-img" src="${item.product_img}">
+            <div class="card-body">
+                <div class="card-content">
+                    <h3 class="product-name">${item.product_name}</h3>
+                    <p class="product-price">${item.product_price}</p>
+                </div>
+                <span class="border-circle">
+                    <span class="icon icon-bag"></span>
+                </span>
+            </div>
+            </article>`;
     });
-    return this.cardBlock.innerHTML += contentProduct;
+    return (this.cardBlock.innerHTML = contentProduct);
   }
 }
