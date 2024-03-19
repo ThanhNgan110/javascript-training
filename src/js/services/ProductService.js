68859c74e7320cac2cf8144e572a7be94502c066
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-19 17:31:40 +0700

    Update productservice

--- src/js/services/ProductService.js
+++ src/js/services/ProductService.js
@@ -1,18 +1,18 @@
-import { api} from "../constants/api";
+import { api } from "../constants/api";
 class ProductService {
   constructor() {}
 
   // function get product
   getProduct = async (res) => {
     try {
-      const data = await fetch(`${api.URL_API}/Product?select=*`, {
+      const res = await fetch(`${api.URL_API}/Product?select=*`, {
         method: "GET",
         headers: {
           apiKey: api.API_KEY,
         },
       });
-      if (res.ok) {
-        const result = data.json();
+      if (res) {
+        const result = await res.json();
         console.log(result);
         return result;
       }
@@ -21,4 +21,4 @@ class ProductService {
     }
   };
 }
-export {ProductService}
+export { ProductService };
