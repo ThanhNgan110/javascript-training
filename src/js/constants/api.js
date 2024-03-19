b84c048f7274fbcd91701e1a8a95d5661051a1da
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-20 00:51:19 +0700

    Add end point

--- src/js/constants/api.js
+++ src/js/constants/api.js
@@ -1,6 +1,7 @@
 const api = {
   URL_API: "https://puyugolpluagelcytrfx.supabase.co/rest/v1",
   API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1eXVnb2xwbHVhZ2VsY3l0cmZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4MTE5NzMsImV4cCI6MjAyNjM4Nzk3M30.3sQ8ZiP3mChvptClo55CDjXosoJa52QB-McugWuoHQo",
+  END_POINT_PRODUCT: "Product?select=*`"
 };
 
 export { api };
