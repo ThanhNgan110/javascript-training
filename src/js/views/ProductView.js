b73daf6e7a00013e8ea2d4511859da838955880d
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-21 09:31:31 +0700

    Update view for funtion search product

--- src/js/views/ProductView.js
+++ src/js/views/ProductView.js
@@ -1,13 +1,22 @@
-import { getElementBySelector } from "../helpers/selector";
+import { querySelector } from "../helpers/selector";
 import { displayProduct } from "../templates/ProductTemplate";
 
 export class ProductView {
   constructor() {
-    this.cardBlock = getElementBySelector(".card-block");
+    this.cardBlock = querySelector(".card-block");
+    this.searchForm = querySelector(".search-form");
+    this.inputSearch = querySelector(".input-search");
   }
 
   renderProduct(products) {
-    // console.log('test',products);
     return (this.cardBlock.innerHTML = displayProduct(products));
+  }
+
+  bindSearchProduct(handler) {
+    this.searchForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(this.inputSearch.value);
+      handler(this.inputSearch.value);
+    });
   }
 }
