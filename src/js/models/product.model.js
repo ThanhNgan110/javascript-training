9e5b32e0b246b47e8219291c33f66bdba53a25c9
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-21 15:17:06 +0700

    Update model product

--- src/js/models/product.model.js
+++ src/js/models/product.model.js
@@ -1,7 +1,6 @@
-import ProductEntity from "../entity/product.entity"
+import ProductEntity from "./entity/product.entity";
 export default class ProductModel {
   createList = (data) => {
-    console.log('test', data);
    return data.map(item => new ProductEntity(item));
   }
 }
