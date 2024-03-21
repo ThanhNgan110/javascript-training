27a6c66509ae8e5e29fbe8884c938b8cb6924b1e
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-21 19:08:11 +0700

    Update function search product

--- src/js/controllers/product.controller.js
+++ src/js/controllers/product.controller.js
@@ -1,20 +1,28 @@
 import ProductService from "../services/product.service";
+
 export default class ProductController {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    this.handleGetProduct();
+    this.init();
   }
 
-  handleGetProduct = async () => {
-    const data = await ProductService.getAllProduct();
-    const products = this.model.createList(data);
-    this.view.renderProduct(products);
+  init = () => {
+    this.renderProducts();
+    this.handleSearch();
   };
 
-  // handleSearch = () => {
-  //   this.view.bindSearchProduct((keyword) => {
-  //     this.model.searchProductByName(keyword);
-  //   });
-  // };
+  renderProducts = async () => {
+    const data = await ProductService.getAllProduct();
+    const products = this.model.createList(data);
+    this.view.renderProductGrid(products);
+  };
+
+  handleSearch = () => {
+   this.view.bindSearchProduct((keyword) => {
+  console.log(keyword);
+   const resultSearch = this.model.searchProductByName(keyword);
+   this.view.renderProductGrid(resultSearch);
+   });
+  };
 }
