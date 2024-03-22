e16180e6e6319ceb7fb8c01a00cbf8b1199eb935
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-22 15:24:00 +0700

    Add function get countpage

--- src/js/models/product.model.js
+++ src/js/models/product.model.js
@@ -8,4 +8,16 @@ export default class ProductModel {
     const searchResults = this.data.filter((product) => product.name.includes(keyword));
     return searchResults;
   }
+
+  getCountPage = () => {
+    const totalProduct = this.data.length;
+    const page_limit = 15;
+    let endPage = totalProduct / page_limit;
+    if(totalProduct % page_limit != 0) {
+      endPage ++;
+    }
+    return endPage;
+  }
+
+
 }
