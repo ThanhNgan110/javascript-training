860ec09570a88138e752538c9d934ecb2352b398
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-28 10:16:57 +0700

    Update main

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,12 @@
-import ProductController from "./controllers/productController";
-import ProductModel from "./models/productModel";
-import ProductView from "./views/productView";
+// import ProductController from "./controllers/productController";
+// import ProductModel from "./models/productModel";
+// import ProductView from "./views/productView";
+// import ProductService from "./services/productService";
 
-const app = new ProductController(new ProductModel(), new ProductView());
+// const app = new ProductController(new ProductModel(), new ProductView(), new ProductService());
+
+import ProductController from "./controllers/productController";
+
+
+const app = new ProductController();
+
