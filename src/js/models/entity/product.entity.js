2567fcad5866baba36319102e1fc7041510eb7a5
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-28 09:33:22 +0700

    update entity product

--- src/js/models/entity/product.entity.js
+++ src/js/models/entity/product.entity.js
@@ -1,9 +1,13 @@
+/** Class representing a product. */
 export default class ProductEntity {
+  /**
+   * Create a product.
+   * @param {number} data - The data contains of object product.
+   */
   constructor(data) {
     this.id = data.id;
     this.name = data.name;
     this.price = data.price;
-    this.imgURL = data.imgURL
+    this.imgURL = data.imgURL;
   }
 }
-
