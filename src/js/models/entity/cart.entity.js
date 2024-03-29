f184042bf6976b883eed7e03b225903324c8cf93
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-03-29 11:40:18 +0700

    Update entity cart

--- src/js/models/entity/cart.entity.js
+++ src/js/models/entity/cart.entity.js
@@ -1,5 +1,11 @@
+/** Class representing a Cart. */
 export default class CartEntity {
+   /**
+   * Create a cart.
+   * @param {number} data - The data contains of object cart.
+   */
   constructor(data) {
+    this.cartId = data.cartId;
     this.id = data.id;
     this.name = data.name;
     this.price = data.price;
