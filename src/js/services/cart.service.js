e66107160d92eabfe5b5a2be22600cbb3f404eb4
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-04-15 10:01:06 +0700

    Update service cart

--- src/js/services/cart.service.js
+++ src/js/services/cart.service.js
@@ -1,85 +1,25 @@
 import { api } from "../constants/config";
-
+import ApiService from "./apiService";
 export default class CartService {
-  /**
-   * Call api for get all product from cart.
-   *
-   * @async
-   * @function getAllProductsFromCart
-   * @return {Promise<string>} The data of product.
-   */
+  constructor() {
+    this.apiService = new ApiService(api.URL_API, api.END_POINT_CART);
+  }
+
   getAllProductsFromCart = async () => {
-    try {
-      const res = await fetch(`${api.URL_API}/${api.END_POINT_CART}`);
-      if (res.ok) {
-        const data = await res.json();
-        return {
-          data,
-          err: null,
-        };
-      }
-    } catch (error) {
-      return {
-        data: null,
-        err: error.message,
-      };
-    }
+   return await this.apiService.get();
+  }
+
+  addProductToCart = async (product) => {
+   await this.apiService.post(product);
   };
 
-  /**
-   * Call api for delete product from cart.
-   *
-   * @async
-   * @function deleteProductFromCart
-   * @return {Promise<string>} The data product .
-   */
+  updateCart = async(product, amount) => {
+    await this.apiService.put(product, {amount:amount});
+  }
+
   deleteProductFromCart = async (id) => {
-    try {
-      const res = await fetch(`${api.URL_API}/${api.END_POINT_CART}/${id}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-      });
-      if (res.ok) {
-        const data = await res.json();
-        return { data, err: null };
-      }
-    } catch (error) {
-      return {
-        data: null,
-        err: error.message,
-      };
-    }
-  };
+    await this.apiService.delete(id);
+  }
 
-  /**
-   * Call api for update quantity for product.
-   *
-   * @async
-   * @function deleteProductFromCart
-   * @return {Promise<string>} The data product from cart.
-   */
-  updateQuantityProduct = async (productId, amount) => {
-    try {
-      const dataProduct = await this.getAllProductsFromCart();
-      const { data } = dataProduct;
-      const product = data.find((product) => {
-        return product.productId === productId;
-      });
-      const res = await fetch(`${api.URL_API}/${api.END_POINT_CART}/${product.id}`,{
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ amount }),
-        }
-      );
-      if (res.ok) {
-        const data = await res.json();
-        return { data, err: null };
-      }
-    } catch (error) {
-      return {
-        data: null,
-        err: error.message
-      }
-    }
-  };
+  
 }
