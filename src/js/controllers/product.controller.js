9cd300f420d2b5d61bcd812af1f8de9f61d2d691
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-04-26 14:21:02 +0700

    Remove func handle add product

--- src/js/controllers/product.controller.js
+++ src/js/controllers/product.controller.js
@@ -1,31 +1,19 @@
-import { showSuccess } from "../utils/toastify";
 import { ALERT_MESSAGE } from "../constants/message";
 import { displayLoading, hideLoading } from "../utils/loading";
 
 import ProductModel from "../models/product.model";
-import CartModel from "../models/cart.model";
 import ProductView from "../views/product.view";
-import CartView from "../views/cart.view";
 import ProductService from "../services/product.service";
-import CartService from "../services/cart.service";
 import CartController from "../controllers/cart.controller";
 
 export default class ProductController {
   constructor() {
     this.model = new ProductModel();
-    this.cartModel = new CartModel();
     this.view = new ProductView();
-    this.cartView = new CartView();
     this.productService = new ProductService();
-    this.cartService = new CartService();
     this.cartController = new CartController();
 
-    // Explicit this binding
     this.view.bindSearchProducts(this.handleSearchProducts);
-    // this.view.bindShowModal();
-    // this.view.bindHiddenModal();
-
-    // Display initial products
     this.handleRenderProductsGrid();
   }
 
@@ -34,33 +22,9 @@ export default class ProductController {
     const res = await this.productService.getAllProducts();
     this.model.setProducts(res);
     this.view.renderProductGrid(this.model.getProducts());
-    this.view.bindAddProducts(this.handleAddProducts);
+    this.view.bindAddProducts(this.cartController.handleAddProduct);
     hideLoading();
-  }
-
-  handleAddProducts = async (productId) => {
-    const products = await this.cartService.getAllProductsFromCart();
-    this.cartModel.setCart(products);
-    const existingProduct = this.cartModel.checkProductIdExisting(productId);
-    const product = this.model.getProductById(productId);
-    if (existingProduct !== undefined) {
-      displayLoading();
-      await this.cartService.updateCart({...existingProduct,amount: existingProduct.amount + 1});
-      hideLoading();
-      showSuccess({ text: ALERT_MESSAGE.ADD_PRODUCT_SUCCESS_MSG });
-    } else {
-      displayLoading();
-      await this.cartService.addProductToCart(product);
-      showSuccess({ text: ALERT_MESSAGE.ADD_PRODUCT_SUCCESS_MSG });
-      hideLoading();
-    }
-    const cart = await this.cartService.getAllProductsFromCart();
-    this.cartModel.setCart(cart);
-    this.cartView.renderCart(this.cartModel.getCart());
-    this.cartView.bindChangeQuantity();
-    this.cartView.bindDeleteProduct(this.cartController.handleDeleteProductFromCart);
-    this.cartView.bindUpdateCart(this.cartController.handleUpdateCart);
-  }
+  };
 
   handleSearchProducts = async (productName) => {
     const products = await this.productService.getAllProducts();
@@ -72,6 +36,5 @@ export default class ProductController {
       this.view.displayMessage("");
     }
     await this.view.renderProductGrid(result);
-  }
+  };
 }
-
