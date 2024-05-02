63b23fcdb841235f089a4929702da9afcf84770d
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-02 15:12:21 +0700

    Add model cart item

---
+++ src/js/models/cartItem.model.js
@@ -0,0 +1,20 @@
+import CartEntity from "./entity/cart.entity";
+export default class CartItemModel {
+  setCart = (products) => {
+    this.products = products.map((item) => new CartEntity(item));
+  };
+
+  getCart = () => {
+    return this.products;
+  };
+
+  checkProductIdExisting(productId) {
+    return this.products
+      ? this.products.find((product) => product.productId === productId)
+      : null;
+  };
+
+  getProductById = (id) => {
+    return this.products.find((item) => item.id === id);
+  };
+}
