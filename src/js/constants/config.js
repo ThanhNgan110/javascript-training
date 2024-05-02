6bb689a939a96ef9ae116a1fced952a96a70e2ba
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-02 15:13:18 +0700

    Update config url

--- src/js/constants/config.js
+++ src/js/constants/config.js
@@ -1,8 +1,9 @@
 const API = {
-    // URL_API: "http://localhost:3000",
-  URL_API: "https://vercel-test-ten-sable-12.vercel.app",
-  END_POINT_PRODUCT: "product",
-  END_POINT_CART:"cart"
+  URL_API: "http://localhost:3000",
+  // URL_API: "https://vercel-test-ten-sable-12.vercel.app",
+  END_POINT_PRODUCT: "products",
+  END_POINT_CART: "carts",
+  END_POINT_CARTITEM: "cartItems",
 };
 
 export { API };
