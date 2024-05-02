abab13cae0fe99445554ecdd25bbeb6952e87bfe
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-02 09:13:48 +0700

    Add func toggle overlay

--- src/js/utils/loading.js
+++ src/js/utils/loading.js
@@ -1,5 +1,6 @@
 import { querySelector } from "../helpers/selector";
 let loader = querySelector(".loading");
+let overlay = querySelector('.overlay');
 
 const displayLoading = () => {
   loader.style.display = 'block';
@@ -9,4 +10,13 @@ const hideLoading = () => {
   loader.style.display = 'none';
 };
 
-export { displayLoading, hideLoading };
+const toggleOverlay = (bool) => {
+  if(bool) {
+    overlay.classList.toggle("hidden", bool)
+  }
+  else {
+     overlay.classList.toggle("hidden", !bool);
+  }
+}
+
+export { displayLoading, hideLoading, toggleOverlay };
