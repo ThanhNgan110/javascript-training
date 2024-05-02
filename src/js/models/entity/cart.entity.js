b7e28cac9c07fd94ba573c61e0aad74124a314db
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-02 15:12:03 +0700

    Update cart entity

--- src/js/models/entity/cart.entity.js
+++ src/js/models/entity/cart.entity.js
@@ -6,11 +6,11 @@ export default class CartEntity {
    */
   constructor(data) {
     this.id = data.id;
-    this.productId = data.productId;
-    this.name = data.name;
-    this.price = data.price;
-    this.amount = data.amount;
-    this.imgURL = data.imgURL;
+    // this.productId = data.productId;
+    // this.name = data.name;
+    // this.price = data.price;
+    // this.amount = data.amount;
+    // this.imgURL = data.imgURL;
   }
 }
 
