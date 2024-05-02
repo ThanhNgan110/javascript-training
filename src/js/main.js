a809efacc995c8d8ec60dac5cd142e8facdf88a8
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-02 15:13:29 +0700

    Update main

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ import ProductController from "./controllers/product.controller";
 import CartController from "./controllers/cart.controller";
 
 const productController = new ProductController();
-const cartController = new CartController();
-const init = { productController, cartController };
+// const cartController = new CartController();
+// const init = { productController, cartController };
+const init = { productController};
 const app = init;
 
