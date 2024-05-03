86ebcabb3ae01bedec498802fe1d8c50883ada01
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-03 11:25:17 +0700

    Update template product

--- src/js/templates/ProductTemplate.js
+++ src/js/templates/ProductTemplate.js
@@ -11,7 +11,7 @@ export const displayProduct = (products) => {
 }
 
 export const productTemplate = (product) => {
-  const { productId, imgURL, name, price } = product;
+  const { id, imgURL, name, price } = product;
   return `
   <article class="card-product">
   <img class="card-img" src="${imgURL}">
@@ -20,7 +20,7 @@ export const productTemplate = (product) => {
           <h3 class="product-name">${name}</h3>
           <p class="product-price">$${price}</p>
       </div>
-      <button data-id=${productId} class="btn-card"> 
+      <button data-id=${id} class="btn-card"> 
         <span class="border-circle"><span class="icon icon-bag"></span></span>
       </button>
   </div>
