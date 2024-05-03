d8ac9297d2e2200772f1beaa7f59cca400f4427f
Thai Thi Thanh Ngan <thaithithanhngaan0199@gmail.com>
2024-05-03 17:41:25 +0700

    Update main

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
 import ProductController from "./controllers/product.controller";
 import CartController from "./controllers/cart.controller";
+import CartModel from "./models/cart.model";
+import CartItemModel from "./models/cartItem.model";
+import ProductModel from "./models/product.model";
+import CartView from "./views/cart.view";
+import ProductView from "./views/product.view";
+import CartService from "./services/cart.service";
+import CartItemService from "./services/cartItem.service";
+import ProductService from "./services/product.service";
 
-const productController = new ProductController();
-// const cartController = new CartController();
-// const init = { productController, cartController };
-const init = { productController};
-const app = init;
+const cartController = new CartController(new CartModel(), new CartItemModel(), new ProductModel(), new CartView(), new ProductView(), new CartService(), new CartItemService());
+const productController = new ProductController(new ProductModel(), new ProductView(), new ProductService(), new CartController());
+const init = { productController, cartController};
+
 
