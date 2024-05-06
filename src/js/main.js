f5f004736688cdbe98340853df0be7dd9bf8780f
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-06 17:38:20 +0700

    Update main

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,23 @@
+// import ProductController from "./controllers/product.controller";
+// import CartController from "./controllers/cart.controller";
+// import CartModel from "./models/cart.model";
+// import CartItemModel from "./models/cartItem.model";
+// import ProductModel from "./models/product.model";
+// import CartView from "./views/cart.view";
+// import ProductView from "./views/product.view";
+// import CartService from "./services/cart.service";
+// import CartItemService from "./services/cartItem.service";
+// import ProductService from "./services/product.service";
+
+// const cartController = new CartController(new CartModel(), new CartItemModel(), new ProductModel(), new CartView(), new ProductView(), new CartService(), new CartItemService());
+// const productController = new ProductController(new ProductModel(), new ProductView(), new ProductService());
+// const init = { productController, cartController};
+
+
 import ProductController from "./controllers/product.controller";
 import CartController from "./controllers/cart.controller";
-import CartModel from "./models/cart.model";
-import CartItemModel from "./models/cartItem.model";
-import ProductModel from "./models/product.model";
-import CartView from "./views/cart.view";
-import ProductView from "./views/product.view";
-import CartService from "./services/cart.service";
-import CartItemService from "./services/cartItem.service";
-import ProductService from "./services/product.service";
 
-const cartController = new CartController(new CartModel(), new CartItemModel(), new ProductModel(), new CartView(), new ProductView(), new CartService(), new CartItemService());
-const productController = new ProductController(new ProductModel(), new ProductView(), new ProductService(), new CartController());
-const init = { productController, cartController};
-
-
+const productController = new ProductController();
+const cartController = new CartController();
+const init = { productController, cartController };
+const app = init;
