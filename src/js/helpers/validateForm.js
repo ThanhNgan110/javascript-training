7fe4849cb9a40440ca25a9b514519cdbb33aae4d
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-10 17:45:36 +0700

    Add func validate form

---
+++ src/js/helpers/validateForm.js
@@ -0,0 +1,50 @@
+import { REGEX_PATTERNS } from "../constants/regexPatterns";
+
+let formError = {};
+const validateEmpty = ({key, value}) => {
+    formError[key] = value.trim() === '' ? `${key} is required` : '';
+};
+
+const validateString = ({key, value}) => {
+    formError[key] = typeof value !== 'string' ?  `${key} must be a string`: '';
+};
+
+const validateInterger = ({key, value}) => {
+  formError[key] = !REGEX_PATTERNS.isInterger.test(value) ? `${key} must be a string` : '';
+};
+
+const validateEmail = ({key, value}) => {
+  formError[key] = !REGEX_PATTERNS.isEmail.test(value) ? `${key} not match format` : '';
+}
+
+/**
+ * Validates the form data
+ * @param {Object} data - The form data
+ * @returns {Object} An object containing validation results
+ */
+function validateForm(validationSchema) {
+  formError = {};
+
+  for (const key in validationSchema) {
+    const { field, value, validators } = validationSchema[key];
+    validateEmpty({ key: field, value });
+    for (const validator of validators) {
+      if (formError[field] !== '') {
+        break;
+      }
+      validator({ key: field, value });
+    }
+  }
+  return { formError }
+}
+
+export {
+  validateString,
+  validateEmpty,
+  validateEmail,
+  validateForm
+};
+
+
+
+
