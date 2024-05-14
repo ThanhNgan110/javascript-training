4158768f2db425c46eed0bc614f490a3490ccc90
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-14 17:25:49 +0700

    Update validate form

--- src/js/helpers/validateForm.js
+++ src/js/helpers/validateForm.js
@@ -1,42 +1,35 @@
 import { REGEX_PATTERNS } from "../constants/regexPatterns";
 
 let formError = {};
-const validateEmpty = ({key, value}) => {
-    formError[key] = value.trim() === '' ? `${key} is required` : '';
+const validateEmpty = ({ key, value }) => {
+  formError[key] = value.trim() === "" ? `${key} is required` : "";
 };
 
-const validateString = ({key, value}) => {
-    formError[key] = typeof value !== 'string' ?  `${key} must be a string`: '';
+const validateString = ({ key, value }) => {
+  formError[key] = typeof value !== "string" ? `${key} must be a string` : "";
 };
 
-const validateInterger = ({key, value}) => {
-  formError[key] = !REGEX_PATTERNS.isInterger.test(value) ? `${key} must be a string` : '';
+const validateInterger = ({ key, value }) => {
+  formError[key] = !REGEX_PATTERNS.isInterger.test(value)
+    ? `${key} must be a string`
+    : "";
 };
 
-const validateEmail = ({key, value}) => {
-  formError[key] = !REGEX_PATTERNS.isEmail.test(value) ? `${key} not match format` : '';
-}
+const validateEmail = ({ key, value }) => {
+  formError[key] = !REGEX_PATTERNS.isEmail.test(value)
+    ? `${key} not match format`
+    : "";
+};
 
-/**
- * Validates the form data
- * @param {Object} data - The form data
- * @returns {Object} An object containing validation results
- */
 function validateForm(validationSchema) {
-  formError = {};
-  for (const key in validationSchema) {
-    // const { field, value, validators } = validationSchema[key];
-    const {key, value} = validationSchema[key];
-    // validateEmpty({ key: field, value });
-    validateEmpty({key: key, value});
-    for (const validator of validators) {
-      if (formError[field] !== '') {
-        break;
-      }
-      validator({ key: field, value });
+  Object.entries(validationSchema).map(([key, value]) => {
+    validateEmpty({ key: key, value: value });
+    if (key === "email") {
+      validateEmail({ key, value });
     }
-  }
-  return { formError }
+  });
+  console.log(formError, "formError");
+  return formError;
 }
 
 export {
@@ -44,9 +37,5 @@ export {
   validateEmpty,
   validateEmail,
   validateForm,
-  validateInterger
+  validateInterger,
 };
-
-
-
-
