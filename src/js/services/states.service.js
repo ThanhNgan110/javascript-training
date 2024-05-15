568c51d13ff0fc8bcca8fa5ad190e5ae1f19ca12
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-15 09:00:39 +0700

    Add states service

---
+++ src/js/services/states.service.js
@@ -0,0 +1,11 @@
+import { API } from "../constants/config";
+import ApiService from "./apiService";
+export default class StatesService {
+  constructor() {
+    this.apiService = new ApiService(API.URL_API, API.END_POINT_STATES);
+  }
+
+  getCart = async () => {
+    return await this.apiService.get();
+  }
+}
