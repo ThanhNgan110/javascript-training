6b29441689b2709df5b2c86535cd98922ec8d568
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-17 17:27:23 +0700

    Update constant for email and phone

--- src/js/constants/regexPatterns.js
+++ src/js/constants/regexPatterns.js
@@ -1,5 +1,5 @@
 export const REGEX_PATTERNS = {
   isInterger: /^\d+$/,
-  isString: /^[a-zA-Z]+$/,
-  isEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  isValidEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  isValidPhone: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 };
