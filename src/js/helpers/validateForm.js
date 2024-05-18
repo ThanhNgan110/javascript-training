fd969a2932631d75c20f2bc564f98e735fc68b16
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-18 17:23:12 +0700

    Update validate form

--- src/js/helpers/validateForm.js
+++ src/js/helpers/validateForm.js
@@ -1,12 +1,18 @@
 import { REGEX_PATTERNS } from "../constants/regexPatterns";
 
 let formError = {};
+
 const validateEmpty = ({ key, value }) => {
   formError[key] = value.trim() === "" ? `${key} is required` : "";
 };
 
 const validateString = ({ key, value }) => {
-  formError[key] = typeof value !== "string" ? `${key} must be a string` : "";
+  console.log(value);
+  console.log(typeof value);
+  formError[key] =
+    typeof value !== "string" || isNaN(value) === false
+      ? `${key} must be a string`
+      : "";
 };
 
 const validateEmail = ({ key, value }) => {
@@ -23,19 +29,24 @@ const validatePhone = ({ key, value }) => {
 
 function validateForm(validationSchema) {
   formError = {};
+
   Object.entries(validationSchema).map(([key, value]) => {
     validateEmpty({ key: key, value: value });
 
     if (!formError[key]) {
+      if (key === "First Name" || key === "Last Name" || key === "Company Name")
+        validateString({ key, value });
+
       if (key === "Email") {
         validateEmail({ key, value });
       }
+
       if (key === "Phone Number") {
         validatePhone({ key, value });
       }
-      
     }
   });
+
   return formError;
 }
 
@@ -44,6 +55,5 @@ export {
   validateEmpty,
   validateEmail,
   validateForm,
-  validateInterger,
   validatePhone,
 };
