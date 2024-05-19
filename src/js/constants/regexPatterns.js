c8bc728deb980706d8cf5c62e2652fb86c818cd3
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-19 22:32:37 +0700

    Remove regex for interger

--- src/js/constants/regexPatterns.js
+++ src/js/constants/regexPatterns.js
@@ -1,5 +1,4 @@
 export const REGEX_PATTERNS = {
-  isInterger: /^\d+$/,
   isValidEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
   isValidPhone: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 };
