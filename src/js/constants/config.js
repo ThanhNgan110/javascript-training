6eb93190884dfe4a283e6fd52b4901fd57a262af
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-20 08:58:05 +0700

    Update url api

--- src/js/constants/config.js
+++ src/js/constants/config.js
@@ -1,6 +1,6 @@
 const API = {
-  URL_API: "http://localhost:3000",
-  // URL_API: "https://json-server-vercel-sigma-orpin.vercel.app",
+  // URL_API: "http://localhost:3000",
+  URL_API: "https://json-server-vercel-sigma-orpin.vercel.app",
   END_POINT_PRODUCT: "products",
   END_POINT_CART: "carts",
   END_POINT_CARTITEM: "cartItems",
