ca62cb9e7766c95cb9c02ea139e7b065a337d130
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-20 17:23:29 +0700

    Add regex for interger

--- src/js/constants/regexPatterns.js
+++ src/js/constants/regexPatterns.js
@@ -1,4 +1,5 @@
 export const REGEX_PATTERNS = {
+  isValidInterger: /^-?\d+$/,
   isValidEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
   isValidPhone: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 };
