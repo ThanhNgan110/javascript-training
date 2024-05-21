3027e32a3d1c37765335edac98c56c5022900301
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-21 15:01:40 +0700

    Update regex for email

--- src/js/constants/regexPatterns.js
+++ src/js/constants/regexPatterns.js
@@ -1,5 +1,5 @@
 export const REGEX_PATTERNS = {
   isValidInterger: /^-?\d+$/,
-  isValidEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+  isValidEmail:  /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/i,
   isValidPhone: /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/
 };
