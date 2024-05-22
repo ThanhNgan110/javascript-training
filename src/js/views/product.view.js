7abbe9a6845e7b506c6f90fce262a80583259c1e
ThanhNgan110 <thaithithanhngaan0199@gmail.com>
2024-05-23 00:05:58 +0700

    Update view product

--- src/js/views/product.view.js
+++ src/js/views/product.view.js
@@ -1,5 +1,5 @@
 import { querySelector } from "../helpers/selector";
-import { displayProduct } from "../templates/ProductTemplate";
+import { displayProduct, cartNumberBadge } from "../templates/ProductTemplate";
 export default class ProductView {
   constructor() {
     this.cardBlock = querySelector(".card-block");
@@ -9,11 +9,16 @@ export default class ProductView {
     this.modal = querySelector(".modal");
     this.btnCloseModal = querySelector(".close-modal");
     this.btnOpenModal = querySelector(".show-modal");
-    this.overlay = querySelector(".overlay");
+    this.blockCart = querySelector(".block-cart");
+
   }
 
-  renderProductGrid(products) {
+  renderProductGrid = (products) => {
     this.cardBlock.innerHTML = displayProduct(products);
+  }
+
+  displayToTalProductAndPrice = (products) => {
+    this.blockCart.innerHTML = cartNumberBadge(products)
   }
 
   bindSearchProducts = (handler) => {
